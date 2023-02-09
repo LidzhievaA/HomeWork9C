cdf3398a739932b4133fe814c8056abd931d5181
Alsu <magadieva.alsu@yandex.ru>
2023-02-09 16:50:57 +0500

    Добавлены решения task 64, 66,68

---
+++ task66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите первое число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int naturalNumbersSum = NaturalNumbersSum(numberN, numberM);
+Console.Write($"M = {numberM}, N = {numberN} -> {naturalNumbersSum}");
+
+int NaturalNumbersSum(int firstNumber, int secondNumber)
+{
+    if (firstNumber <= 0) firstNumber = 1;
+    if (secondNumber <= 0) secondNumber = 1;
+    if (firstNumber == secondNumber) return firstNumber;
+    if (firstNumber < secondNumber) return firstNumber + NaturalNumbersSum(firstNumber + 1, secondNumber);
+    if (firstNumber > secondNumber) return firstNumber + NaturalNumbersSum(firstNumber - 1, secondNumber);
+    return firstNumber;
+}
---
+++ task68/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите первое неотрицательное число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе неотрицательное число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM < 0 || numberN < 0) Console.WriteLine("Некорректные данные! Необходимо ввести положительные числа!");
+else
+{
+int ackermannFunction = AckermannFunction (numberM, numberN);
+Console.WriteLine($" M = {numberM}, N = {numberN} -> A(M,N) = {ackermannFunction}");
+}
+
+int AckermannFunction (int numM, int numN)
+{
+    if (numM == 0) return numN + 1;
+    if (numM > 0 && numN == 0) return AckermannFunction (numM - 1, 1);
+    if (numM > 0 && numN > 0) return AckermannFunction (numM - 1, AckermannFunction(numM, numN - 1));
+    return 0;
+}
